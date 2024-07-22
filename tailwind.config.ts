2c5bd019b07c6308587b54c077ff6b5318018c94
Manuel Muñoz-González <72055569+manu-mg@users.noreply.github.com>
2024-07-22 14:49:51 -0400

    chore: Add Skeleton for theming and components
    Modify tailwind.config to use Skeleton plugins also

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+
+import { join } from 'path'
+import { skeleton } from '@skeletonlabs/skeleton/plugin'
+import * as themes from '@skeletonlabs/skeleton/themes' // Import the themes
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        join(
+            require.resolve('@skeletonlabs/skeleton-react'),
+            '../**/*.{html,js,jsx,tsx,ts}'
+        ),
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+        },
     },
-  },
-  plugins: [],
-};
-export default config;
+    plugins: [
+        skeleton({
+            themes: [
+                themes.cerberus,
+                themes.rose,
+                themes.pine,
+                themes.catppuccin,
+            ],
+        }),
+    ],
+}
+export default config

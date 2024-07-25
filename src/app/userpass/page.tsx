d64f68689a7372141e0917974f1e4f88ec521c1d
Manuel Muñoz-González <72055569+manu-mg@users.noreply.github.com>
2024-07-25 15:12:16 -0400

    Enlarge form for user-pass

--- src/app/userpass/page.tsx
+++ src/app/userpass/page.tsx
@@ -7,10 +7,10 @@ export default function UserPass() {
         <main className="relative grid justify-items-center">
             <div style={{ paddingTop: '8%' }}> {/* Spacer */}</div>
             <Backarrow ruta="/" />
-            <form className="bg-surface-100-900 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <form className="bg-surface-100-900 shadow-md rounded px-8 pt-6 pb-8 mb-4 w-2/3">
                 <div className="mb-6">
                     <label
-                        className="block text-surface-500 text-sm font-bold mb-2"
+                        className="block text-xl text-surface-500 font-bold my-2"
                         htmlFor="password"
                     >
                         RUN Cliente
@@ -21,14 +21,14 @@ export default function UserPass() {
                         type="text" // type="password"
                         placeholder="11.111.111-1"
                     />
-                    <p className="text-error-500 text-xs italic">
-                        Ingresa tu RUN.
-                    </p>
+                    <p className="text-error-500 italic">Ingresa tu RUN.</p>
                 </div>
+
+                <br className="h-5" />
 
                 <div className="mb-4">
                     <label
-                        className="block text-surface-500 text-sm font-bold mb-2"
+                        className="block text-xl text-surface-500 font-bold mb-2"
                         htmlFor="username"
                     >
                         Número de Orden
@@ -43,7 +43,7 @@ export default function UserPass() {
 
                 <div className="flex items-center justify-between">
                     <button
-                        className="bg-primary-500 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        className="bg-primary-500 hover:bg-primary-700 text-white text-xl font-bold mt-4 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                         type="button"
                     >
                         Verificar
